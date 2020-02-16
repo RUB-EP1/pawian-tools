24de062a3a4075bc273f0841aef439526f129a9d
Remco de Boer <redeboer@ep1.ruhr-uni-bochum.de>
2020-02-16 16:50:48 +0100

    test: improve write DataFrame comparison (!8)

--- pawian/data/test/test_write_data.py
+++ pawian/data/test/test_write_data.py
@@ -1,6 +1,8 @@
 from os import remove
 from os.path import dirname, realpath
-from numpy import allclose, isclose
+from pandas.testing import assert_frame_equal
+import pytest
+
 from pawian.data import read_ascii
 
 
@@ -10,69 +12,14 @@ INPUT_FILE_MC = f'{SCRIPT_DIR}/momentum_tuples_mc.dat'
 OUTPUT_FILE = f'{SCRIPT_DIR}/temp_output.dat'
 
 
-def test_write_no_weights():
-    frame_in = read_ascii(INPUT_FILE_MC, particles=3)
-    frame_in.loc[:9].pawian.write_ascii(OUTPUT_FILE)
-    frame_out = read_ascii(OUTPUT_FILE, particles=3)
-    assert len(frame_out) == 10
-    assert len(frame_out.columns.levels[0]) == 3
-    assert len(frame_out.columns.levels[1]) == 4
-
-    embedded_particles = frame_out.columns.droplevel(1).unique().tolist()
-    tol = 1e-04
-    head = frame_out.head(1)
-    assert allclose(
-        head[embedded_particles[0]].values.tolist()[0],
-        [-0.279306, -0.175576, -0.122704, 0.378648],
-        rtol=tol)
-    assert allclose(
-        head[embedded_particles[2]].values.tolist()[0],
-        [0.0245524, -0.146873, 0.193947, 1.88553],
-        rtol=tol)
-
-    tail = frame_out.tail(1)
-    assert allclose(
-        tail[embedded_particles[0]].values.tolist()[0],
-        [-0.12336, 0.134471, -0.0713491, 0.240563],
-        rtol=tol)
-    assert allclose(
-        tail[embedded_particles[2]].values.tolist()[0],
-        [0.609579, -0.1528, 0.363307, 2.00558],
-        rtol=tol)
-
-    remove(OUTPUT_FILE)
-
-
-def test_write_with_weights():
-    frame_in = read_ascii(INPUT_FILE_DATA, particles=3)
-    frame_in.loc[:9].pawian.write_ascii(OUTPUT_FILE)
-    frame_out = read_ascii(OUTPUT_FILE, particles=3)
-    assert len(frame_out) == 10
-    assert len(frame_out.columns.levels[0]) == 4
-    assert len(frame_out.columns.levels[1]) == 5
-    assert '' in frame_out.columns.levels[1]
-
-    column_names = frame_out.columns.droplevel(1).unique().tolist()
-    tol = 1e-06
-    head = frame_out.head(1)
-    assert isclose(head[column_names[-1]].values[0], 0.99407, rtol=tol)
-    assert allclose(
-        head[column_names[0]].values.tolist()[0],
-        [-0.00357645, 0.0962561, 0.0181079, 0.170545],
-        rtol=tol)
-    assert allclose(
-        head[column_names[2]].values.tolist()[0],
-        [-0.174404, -0.719412, -0.233159, 2.0243],
-        rtol=tol)
-
-    tail = frame_out.tail(1)
-    assert allclose(
-        tail[column_names[0]].values.tolist()[0],
-        [0.0752243, -0.034057, 0.033107, 0.1655130],
-        rtol=tol)
-    assert allclose(
-        tail[column_names[2]].values.tolist()[0],
-        [-0.710628, 0.1081589, -0.2437929, 2.01781],
-        rtol=tol)
-
+@pytest.mark.parametrize('input_file', [
+    INPUT_FILE_DATA,
+    INPUT_FILE_MC,
+])
+def test_write_no_weights(input_file):
+    particles = ['pi+', 'D0', 'D-']
+    frame_in = read_ascii(input_file, particles=particles).loc[:9]
+    frame_in.pawian.write_ascii(OUTPUT_FILE)
+    frame_out = read_ascii(OUTPUT_FILE, particles=particles)
+    assert_frame_equal(frame_in, frame_out)
     remove(OUTPUT_FILE)
