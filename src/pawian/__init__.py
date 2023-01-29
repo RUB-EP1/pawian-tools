f23079d56a0ed856a27375a9402c57b58e47ee22
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-01-29 03:30:53 +0100

    FIX: update links to Pawian documentation
    https://github.com/redeboer/PawianTools/actions/runs/4034692579/jobs/6936095659

--- src/pawian/__init__.py
+++ src/pawian/__init__.py
@@ -1,7 +1,7 @@
 """The PawianTools package.
 
 A collection of Python tools that facilitate working with `Pawian
-<https://panda-wiki.gsi.de/foswiki/bin/view/PWA/PawianPwaSoftware>`__ output.
+<https://panda-wiki.gsi.de/bin/view/PWA/PawianPwaSoftware>`__ output.
 Note that the tools here **are specific to Pawian**. Any tools that are more
 general in usage should be extracted to a separate module.
 """
